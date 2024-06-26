4f4671b14ef38cef48daf37a9238b05321d24d67
Evian <allainevan1@gmail.com>
2024-06-26 13:30:36 +0200

    AuthContext + Authentification

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -56,11 +56,16 @@ export const login = async (req, res) => {
 
     const age = 1000 * 60 * 60 * 24 * 7;
 
-    const token = jwt.sign({
-        id:user.id
-    }, process.env.JWT_SECRET_KEY, {
+    const token = jwt.sign(
+        {
+        id:user.id,
+        isAdmin: false,
+    }, 
+    process.env.JWT_SECRET_KEY, {
         expiresIn: age
     });
+
+    const {password: userPassword, ...userInfo} = user
 
     res
     .cookie("token", token, {
@@ -69,7 +74,7 @@ export const login = async (req, res) => {
         maxAge: age,
     })
     .status(200)
-    .json({ message: 'Login successful!' });
+    .json(userInfo);
     } catch(err) {
         console.log(err);
         res.status(500).json({ message: 'Failed to login!' });
